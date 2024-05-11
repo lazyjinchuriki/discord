930cb858d328c9ba1411b0a22e562cc9714aa3a1
Rahul Khushalani <rahulkhushalani@proton.me>
2024-05-11 22:22:14 +0530

    added a cool bg to login pages and removed .env from gitignore
    .env must be removed in future as of now this repo is private

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,16 @@
+import discordBg from "@/assets/discordBg.png";
+import Image from "next/image";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen flex items-center justify-center">{children}</div>
+    <div className="h-screen flex items-center justify-center">
+      <Image
+        src={discordBg}
+        layout="fill"
+        objectFit="cover"
+        alt="Discord background"
+      />
+      {children}
+    </div>
   );
 };
 
